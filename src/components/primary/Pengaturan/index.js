03454e743a8cbe33c0dd370c8d7ffe56d84e42f1
Melanie Safira <melaniesafira94@gmail.com>
2021-06-23 21:47:24 +0700

    Tambah menu help

--- src/components/primary/Pengaturan/index.js
+++ src/components/primary/Pengaturan/index.js
@@ -1,15 +1,26 @@
 import React from 'react';
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, Linking } from 'react-native';
 import { colors, fonts } from '../../../utils';
 
 export default function Pengaturan({ onPressTitle, title }) {
+  const openGdrive = () => {
+    Linking.openURL('https://drive.google.com/file/d/1xfgOp55GLrISIegPlztIf2PI76oSCehb/view?usp=sharing');
+  };
+
   return (
     <View style={styles.container}>
       <View style={styles.content}>
-        <Text style={styles.contentText}>Versi 0.0.1</Text>
-        <TouchableOpacity onPress={ onPressTitle } style={styles.button}>
-          <Text style={styles.buttonText}>{ title }</Text>
+        <TouchableOpacity onPress={() => openGdrive()} style={styles.buttonHelp}>
+          <Text style={styles.buttonHelpText}>Help</Text>
         </TouchableOpacity>
+
+        <View>
+          <Text style={styles.contentText}>Versi 0.0.1</Text>
+
+          <TouchableOpacity onPress={ onPressTitle } style={styles.button}>
+            <Text style={styles.buttonText}>{ title }</Text>
+          </TouchableOpacity>
+        </View>
       </View>
     </View>
   );
@@ -21,8 +32,22 @@ const styles = StyleSheet.create({
     alignItems: 'center'
   },
   content: {
+    flex: 1,
     width: '90%',
-    marginTop: 20
+    marginTop: 20,
+    justifyContent: 'space-between'
+  },
+  buttonHelp: {
+    backgroundColor: colors.white,
+    padding: 13,
+    borderRadius: 10,
+    elevation: 5
+  },
+  buttonHelpText: {
+    fontSize: 16,
+    color: colors.green1,
+    textAlign: 'center',
+    fontFamily: fonts.primary[700]
   },
   contentText: {
     fontSize: 15,
@@ -32,15 +57,16 @@ const styles = StyleSheet.create({
     fontFamily: fonts.primary.normal
   },
   button: {
-    backgroundColor: colors.white,
+    backgroundColor: colors.green1,
     padding: 13,
-    borderRadius: 10,
-    elevation: 5
+    borderRadius: 50,
+    elevation: 2,
+    marginBottom: 20
   },
   buttonText: {
     fontSize: 18,
-    color: colors.green1,
+    color: colors.white,
     textAlign: 'center',
     fontFamily: fonts.primary[800]
-  }
+  },
 });
